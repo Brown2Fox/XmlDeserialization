58fe06e811d155fd6bd142a3e9dcd12f2697e6c9
Andrey Akimov <a.a.m.incorp@yandex.ru>
2018-06-17 23:37:35 +0300

    Added show help message option -h

--- XmlDeserialization/src/ProgramOpts.cs
+++ XmlDeserialization/src/ProgramOpts.cs
@@ -6,18 +6,24 @@ namespace XmlDeserialization
     public class ProgramOpts
     {
         public static string InputDirectory = ".";
+        private static bool NeedShowHelp = false;
         
         private static OptionSet optSet = new OptionSet()
         {
             {
-                "d|dir=", "Input directory name.",
+                "d|dir=", "Input directory name (Default directory is a current directory)",
                 v => InputDirectory = v
+            },
+            {
+                "h|help", "Show help",
+                v => NeedShowHelp = !string.IsNullOrEmpty(v)
             }
         };
         
         public static void ParseArgs(string[] args)
         {
             optSet.Parse(args);
+            if (NeedShowHelp) throw new OptionException("-- Help Message --", "h|help");
         }
 
         public static void HelpMessage()
